925fd23c37e285f3191187b154d9c8302082d747
Aniruddha <aniruddhavikharankarfb@gmail.com>
2023-02-04 14:00:40 +0530

    artifacts included

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import safeWillPng from "../../../public/images/Safewill.png";
+import safeWillPng from "../../../public/images/safewill.png";
 import Image from "next/image";
 
 const Footer = () => {
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import React, { useEffect } from "react";
 import { ethers } from "ethers";
 import Image from "next/image";
-import safeWillPng from "../../../public/images/Safewill.png";
+import safeWillPng from "../../../public/images/safewill.png";
 import polygonPng from "../../../public/images/polygon.png";
 import ethereumPng from "../../../public/images/ethereum.png";
 import filPng from "../../../public/images/fil.png";
